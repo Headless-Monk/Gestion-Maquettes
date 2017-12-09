8df771b64a5703d7f2f8c9f309193b1cbd858975
Sylvain GHERARDI <sylvain.gherardi96@live.fr>
2017-12-09 13:25:41 +0100

    h formation + maquette

--- formation.h
+++ formation.h
@@ -10,14 +10,16 @@ class formation
 {
 public:
   formation();
+  formation(d_libelle);
+  formation(std::vector <maquette> &maquettes, std::string mention); //vraiment nécéssaire ?
+  formation(formation &f);
+
   ~formation();
 
 
 private:
   std::vector <maquette> d_maquettes;
-
-  std::string d_libelle;
-
+  std::string d_mention; //peut changer en fonction de ce qu'on veut stocker dans une formation
 };
 
 #endif
--- maquette.h
+++ maquette.h
@@ -10,14 +10,21 @@ class maquette
 {
 public:
   maquette();
+  maquette(std::string mention, std::string parcours, unsigned int annee, unsigned int semestre);
+  maquette(std::vector <ue*> &ues, std::string mention, std::string parcours, unsigned int annee, unsigned int semestre); //vraiment nécessaire ?
+  maquette(maquette &m);
   ~maquette();
+  //ajout d'un constructeur avec d_domaine ? plus logique pour implémentation en c++ mais inutile pour utilisation du programme
 
 
 private:
   std::vector <ue*> d_ues;
 
-  unsigned int d_num_semestre; //1 à 6 pour une licence
-  std::string d_libelle;
+  std::string d_domaine; //défaut = "SCIENCES ET TECHNOLOGIE, SANTE"
+  std::string d_mention;
+  std::string d_parcours;
+  unsigned int d_annee;
+  unsigned int d_semestre;
 
 };
 
