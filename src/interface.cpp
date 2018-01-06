ee0b8c0ad4e26faef1e6b67bf4ec827daf820558
Olivier AGUDO PEREZ <olivier.agudo-perez@uha.fr>
2018-01-06 14:35:25 +0100

    interface

--- src/interface.cpp
+++ src/interface.cpp
@@ -38,8 +38,6 @@ void interface::initialiser()
                 afficher_menu_UEs();
                 break;
             case 4:
-                system("cls");
-                afficher_menu_ECUEs();
                 break;
             default:
                 break;
@@ -69,16 +67,18 @@ void interface::afficher_menu_maquettes()
     switch (choix)
     {
         case 1:
+            system("cls");
             afficher_creer_maquette();
             break;
         case 2:
+            system("cls");
             afficher_modifier_maquette();
             break;
         case 3:
+            system("cls");
             afficher_supprimer_maquette();
             break;
         case 4:
-            afficher_menu_principal();
             break;
         default:
             break;
@@ -87,17 +87,95 @@ void interface::afficher_menu_maquettes()
 
 void interface::afficher_menu_formations()
 {
+    std::cout << "1) Creer maquette" << std::endl;
+    std::cout << "2) Modifier maquette" << std::endl;
+    std::cout << "3) Supprimer maquette" << std::endl;
+    std::cout << "4) Retour" << std::endl;
 
+    int choix = 0;
+    std::cin >> choix;
+
+    switch (choix)
+    {
+        case 1:
+            system("cls");
+            afficher_creer_formation();
+            break;
+        case 2:
+            system("cls");
+            afficher_modifier_formation();
+            break;
+        case 3:
+            system("cls");
+            afficher_supprimer_formation();
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
 }
 
 void interface::afficher_menu_UEs()
 {
+    std::cout << "1) Creer UE" << std::endl;
+    std::cout << "2) Modifier UE" << std::endl;
+    std::cout << "3) Supprimer UE" << std::endl;
+    std::cout << "4) Retour" << std::endl;
 
+    int choix = 0;
+    std::cin >> choix;
+
+    switch (choix)
+    {
+        case 1:
+            system("cls");
+            afficher_creer_UE();
+            break;
+        case 2:
+            system("cls");
+            afficher_modifier_UE();
+            break;
+        case 3:
+            system("cls");
+            afficher_supprimer_UE();
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
 }
 
 void interface::afficher_menu_ECUEs()
 {
+    std::cout << "1) Creer ECUE" << std::endl;
+    std::cout << "2) Modifier ECUE" << std::endl;
+    std::cout << "3) Supprimer ECUE" << std::endl;
+    std::cout << "4) Retour" << std::endl;
 
+    int choix = 0;
+    std::cin >> choix;
+
+    switch (choix)
+    {
+        case 1:
+            system("cls");
+            afficher_creer_ECUE();
+            break;
+        case 2:
+            system("cls");
+            afficher_modifier_ECUE();
+            break;
+        case 3:
+            system("cls");
+            afficher_supprimer_ECUE();
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
 }
 
 void interface::afficher_creer_maquette()
@@ -111,6 +189,51 @@ void interface::afficher_modifier_maquette()
 }
 
 void interface::afficher_supprimer_maquette()
+{
+
+}
+
+void interface::afficher_creer_formation()
+{
+
+}
+
+void interface::afficher_modifier_formation()
+{
+
+}
+
+void interface::afficher_supprimer_formation()
+{
+
+}
+
+void interface::afficher_creer_UE()
+{
+
+}
+
+void interface::afficher_modifier_UE()
+{
+
+}
+
+void interface::afficher_supprimer_UE()
+{
+
+}
+
+void interface::afficher_creer_ECUE()
+{
+
+}
+
+void interface::afficher_modifier_ECUE()
+{
+
+}
+
+void interface::afficher_supprimer_ECUE()
 {
 
 }
--- src/interface.h
+++ src/interface.h
@@ -19,6 +19,18 @@ public:
     void afficher_creer_maquette();
     void afficher_modifier_maquette();
     void afficher_supprimer_maquette();
+
+    void afficher_creer_formation();
+    void afficher_modifier_formation();
+    void afficher_supprimer_formation();
+
+    void afficher_creer_UE();
+    void afficher_modifier_UE();
+    void afficher_supprimer_UE();
+
+    void afficher_creer_ECUE();
+    void afficher_modifier_ECUE();
+    void afficher_supprimer_ECUE();
 };
 
 #endif
