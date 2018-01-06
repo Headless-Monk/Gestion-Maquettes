672bd40f8ebd3047eb1ef82391f9164a2065f255
Olivier AGUDO PEREZ <olivier.agudo-perez@uha.fr>
2018-01-06 21:44:47 +0100

    interface

--- src/interface.h
+++ src/interface.h
@@ -24,26 +24,31 @@ public:
     void afficher_menu_UEs();
     void afficher_menu_ECUEs();
 
+    //SYLVAIN
     void afficher_creer_maquette();
     void afficher_modifier_maquette();
     void afficher_supprimer_maquette();
     void afficher_liste_maquette();
 
+    //OLIVIER
     void afficher_creer_formation();
     void afficher_modifier_formation();
     void afficher_supprimer_formation();
     void afficher_liste_formation();
 
+    //TAREK
     void afficher_creer_UE();
     void afficher_modifier_UE();
     void afficher_supprimer_UE();
     void afficher_liste_UE();
 
+    //OLIVIER
     void afficher_creer_ECUE();
     void afficher_modifier_ECUE();
     void afficher_supprimer_ECUE();
     void afficher_liste_ECUE();
 
+    //QUAND LE RESTE SERA FINI
     void afficher_creer_UE_choix();
     void afficher_modifier_UE_choix();
     void afficher_supprimer_UE_choix();
