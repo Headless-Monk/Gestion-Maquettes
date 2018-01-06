21c9e67452716b918964357ba37432a71f9aa840
Olivier AGUDO PEREZ <olivier.agudo-perez@uha.fr>
2018-01-06 16:30:33 +0100

    interface

--- src/interface.cpp
+++ src/interface.cpp
@@ -15,7 +15,7 @@ interface::~interface()
 void interface::initialiser()
 {
     int choix = 0;
-    while(choix != 4)
+    while(choix != 5)
     {
         std::cout << std::endl;
 
@@ -33,6 +33,11 @@ void interface::initialiser()
                 break;
             case 3:
                 afficher_menu_UEs();
+                break;
+            case 4:
+                afficher_menu_ECUEs();
+                break;
+            case 5:
                 break;
             default:
                 break;
@@ -52,109 +57,129 @@ void interface::afficher_menu_principal()
 
 void interface::afficher_menu_maquettes()
 {
-    system("cls");
-    std::cout << "1) Creer maquette" << std::endl;
-    std::cout << "2) Modifier maquette" << std::endl;
-    std::cout << "3) Supprimer maquette" << std::endl;
-    std::cout << "4) Retour" << std::endl;
-
     int choix = 0;
-    std::cin >> choix;
-
-    switch (choix)
+    while(choix != 4)
     {
-        case 1:
-            afficher_creer_maquette();
-            break;
-        case 2:
-            afficher_modifier_maquette();
-            break;
-        case 3:
-            afficher_supprimer_maquette();
-            break;
-        default:
-            break;
+        system("cls");
+        std::cout << "1) Creer maquette" << std::endl;
+        std::cout << "2) Modifier maquette" << std::endl;
+        std::cout << "3) Supprimer maquette" << std::endl;
+        std::cout << "4) Retour" << std::endl;
+
+        std::cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                afficher_creer_maquette();
+                break;
+            case 2:
+                afficher_modifier_maquette();
+                break;
+            case 3:
+                afficher_supprimer_maquette();
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
     }
 }
 
 void interface::afficher_menu_formations()
 {
-    system("cls");
-    std::cout << "1) Creer maquette" << std::endl;
-    std::cout << "2) Modifier maquette" << std::endl;
-    std::cout << "3) Supprimer maquette" << std::endl;
-    std::cout << "4) Retour" << std::endl;
-
     int choix = 0;
-    std::cin >> choix;
-
-    switch (choix)
+    while(choix != 4)
     {
-        case 1:
-            afficher_creer_formation();
-            break;
-        case 2:
-            afficher_modifier_formation();
-            break;
-        case 3:
-            afficher_supprimer_formation();
-            break;
-        default:
-            break;
+        system("cls");
+        std::cout << "1) Creer maquette" << std::endl;
+        std::cout << "2) Modifier maquette" << std::endl;
+        std::cout << "3) Supprimer maquette" << std::endl;
+        std::cout << "4) Retour" << std::endl;
+
+        std::cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                afficher_creer_formation();
+                break;
+            case 2:
+                afficher_modifier_formation();
+                break;
+            case 3:
+                afficher_supprimer_formation();
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
     }
 }
 
 void interface::afficher_menu_UEs()
 {
-    system("cls");
-    std::cout << "1) Creer UE" << std::endl;
-    std::cout << "2) Modifier UE" << std::endl;
-    std::cout << "3) Supprimer UE" << std::endl;
-    std::cout << "4) Retour" << std::endl;
-
     int choix = 0;
-    std::cin >> choix;
-
-    switch (choix)
+    while(choix != 4)
     {
-        case 1:
-            afficher_creer_UE();
-            break;
-        case 2:
-            afficher_modifier_UE();
-            break;
-        case 3:
-            afficher_supprimer_UE();
-            break;
-        default:
-            break;
+        system("cls");
+        std::cout << "1) Creer UE" << std::endl;
+        std::cout << "2) Modifier UE" << std::endl;
+        std::cout << "3) Supprimer UE" << std::endl;
+        std::cout << "4) Retour" << std::endl;
+
+        std::cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                afficher_creer_UE();
+                break;
+            case 2:
+                afficher_modifier_UE();
+                break;
+            case 3:
+                afficher_supprimer_UE();
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
     }
 }
 
 void interface::afficher_menu_ECUEs()
 {
-    system("cls");
-    std::cout << "1) Creer ECUE" << std::endl;
-    std::cout << "2) Modifier ECUE" << std::endl;
-    std::cout << "3) Supprimer ECUE" << std::endl;
-    std::cout << "4) Retour" << std::endl;
-
     int choix = 0;
-    std::cin >> choix;
-
-    switch (choix)
+    while(choix != 4)
     {
-        case 1:
-            afficher_creer_ECUE();
-            break;
-        case 2:
-            afficher_modifier_ECUE();
-            break;
-        case 3:
-            afficher_supprimer_ECUE();
-            break;
-        default:
-            break;
+        system("cls");
+        std::cout << "1) Creer ECUE" << std::endl;
+        std::cout << "2) Modifier ECUE" << std::endl;
+        std::cout << "3) Supprimer ECUE" << std::endl;
+        std::cout << "4) Retour" << std::endl;
+
+        std::cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                afficher_creer_ECUE();
+                break;
+            case 2:
+                afficher_modifier_ECUE();
+                break;
+            case 3:
+                afficher_supprimer_ECUE();
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
     }
 }
 
