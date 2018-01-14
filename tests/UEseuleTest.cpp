57aca937787eb454149ac3b3c2d1391b2c700a29
Chaaban Bilal <linsomniak96@gmail.com>
2018-01-14 12:23:57 +0100

    Suite des tests UeSeule

--- tests/UEseuleTest.cpp
+++ tests/UEseuleTest.cpp
@@ -4,26 +4,35 @@
 #include <vector>
 
 
-void UESeuleALesBonnesValeurs(const UEseule& ues,int heures_cm,heures_td,heures_tp,
-                              std::string code, std::string intitule, int credits, int coefficient){
+void ueSeuleALesBonnesValeurs(const UEseule& ues,unsigned int heures_cm,unsigned int heures_td, unsigned intheures_tp,
+                              std::string code, std::string intitule, unsigned int credits, unsigned int coefficient)
+{
 
-        REQUIRE(ues.heures_cm()==heures_cm);
-        REQUIRE(ues.heures_td()==heures_td);
-        REQUIRE(ues.heures_tp()==heures_tp);
-        REQUIRE(ues.code()==code);
-        REQUIRE(ues.intitule()==intitule);
-        REQUIRE(ues.credits()==credits);
-        REQUIRE(ues.coefficient()==coefficient);
+    REQUIRE(ues.heures_cm()==heures_cm);
+    REQUIRE(ues.heures_td()==heures_td);
+    REQUIRE(ues.heures_tp()==heures_tp);
+    REQUIRE(ues.code()==code);
+    REQUIRE(ues.intitule()==intitule);
+    REQUIRE(ues.credits()==credits);
+    REQUIRE(ues.coefficient()==coefficient);
+}
+
+void ueSeuleALesBonneHeuresDonnees(const UEseule& ues,unsigned int heures_cm,unsigned int heures_td, unsigned int heures_tp)
+{
+    REQUIRE(ues.heures_cm()==heures_cm);
+    REQUIRE(ues.heures_td()==heures_td);
+    REQUIRE(ues.heures_tp()==heures_tp);
+
 }
 
 TEST_CASE("L'UE seule est bien cree", "[UEseule]")
 {
 
-    int heures_cm = 3;
-    int heures _td = 2;
-    int heures_tp = 1;
-    int credits = 3;
-    int coefficient = 1;
+    unsigned int heures_cm = 3;
+    unsigned int heures_td = 2;
+    unsigned int heures_tp = 1;
+    unsigned int credits = 3;
+    unsigned int coefficient = 1;
     std::string code = "13GUPRG5";
     std::string intitule = "Anglais";
 
@@ -34,35 +43,57 @@ TEST_CASE("L'UE seule est bien cree", "[UEseule]")
 
     SECTION("Les donnees d'Ue seule construite sont correctes")
     {
-       UESeuleALesBonnesValeurs(ues,heures_cm,heures_td,heures_tp,code,intitule,credits,coefficient);
+       ueSeuleALesBonnesValeurs(ues,heures_cm,heures_td,heures_tp,code,intitule,credits,coefficient);
     }
     SECTION("Les donnees d'UE seule construite par recopie sont correctes")
     {
-       UESeuleALesBonnesValeurs(ues2,heures_cm,heures_td,heures_tp,code,intitule,credits,coefficient);
+       ueSeuleALesBonnesValeurs(ues2,heures_cm,heures_td,heures_tp,code,intitule,credits,coefficient);
     }
 
 }
 
 TEST_CASE("L'UE seule est bien modifiee")
 {
+
+    unsigned int heures_cm = 3;
+    unsigned int heures_td = 2;
+    unsigned int heures_tp = 1;
+    unsigned int credits = 3;
+    unsigned int coefficient = 1;
+    std::string code = "13GUPRG5";
+    std::string intitule = "Anglais";
+
+    UEseule ues(heures_cm,heures_td,heures_tp,code,intitule,3,1);
+
     SECTION(" L'heure des cm modifiee a la bonne valeur")
     {
+        ues.heures_cm(4);
+        ueSeuleALesBonneHeuresDonnees(ues,4,heures_td,heures_tp);
+
 
     }
 
     SECTION ("L'heure des td modifiee a la bonne valeur")
     {
-
+        ues.heures_td(5);
+        ueSeuleALesBonneHeuresDonnees(ues,heures_cm,5,heures_tp);
     }
 
     SECTION("L'heure des tp modifiee a la bonne valeur")
     {
-
+        ues.heures_td(6);
+        ueSeuleALesBonneHeuresDonnees(ues,heures_cm,heures_td,6);
+    }
+    SECTION ("Le total des heures a la bonne valeur")
+    {
+        unsigned int total = heures_cm + heures_td + heures_tp;
+        REQUIRE(ues.duree_totale(),total);
     }
 
-    SECTION ("L'UE seule modifé a les bonnes valeurs")
+    SECTION ("L'UE seule modifee a les bonnes valeurs")
     {
-
+        ues.modifier_completement_ue(1,1,1,"13GUPRG6","Allemand",1,2);
+        ueSeuleALesBonnesValeurs(ues,1,1,1,"13GUPRG6","Allemand",1,2);
     }
 
 }
